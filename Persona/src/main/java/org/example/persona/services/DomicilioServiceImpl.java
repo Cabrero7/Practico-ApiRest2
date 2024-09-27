7f71ac7659b721f252a70d71208ff8d958ebbe2e
Cabrero7 <dcabrero761@gmail.com>
2024-09-27 12:49:33 -0300

    DomicilioServiceImpl.java

--- Persona/src/main/java/org/example/persona/services/DomicilioServiceImpl.java
+++ Persona/src/main/java/org/example/persona/services/DomicilioServiceImpl.java
@@ -2,7 +2,9 @@ package org.example.persona.services;
 
 import org.example.persona.entities.Domicilio;
 import org.example.persona.repositories.BaseRepository;
+import org.springframework.stereotype.Service;
 
+@Service
 public class DomicilioServiceImpl extends BaseServiceImpl<Domicilio, Long> implements DomicilioService{
     public DomicilioServiceImpl(BaseRepository<Domicilio, Long> baseRepository) {
         super(baseRepository);
