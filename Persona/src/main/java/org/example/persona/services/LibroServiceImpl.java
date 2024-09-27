b53a0c06214728b905236aa5bd48d9c6dacf2f0a
Cabrero7 <dcabrero761@gmail.com>
2024-09-27 12:50:06 -0300

    LibroServiceImpl.java

--- Persona/src/main/java/org/example/persona/services/LibroServiceImpl.java
+++ Persona/src/main/java/org/example/persona/services/LibroServiceImpl.java
@@ -2,8 +2,9 @@ package org.example.persona.services;
 
 import org.example.persona.entities.Libro;
 import org.example.persona.repositories.BaseRepository;
+import org.springframework.stereotype.Service;
 
-
+@Service
 public class LibroServiceImpl extends BaseServiceImpl<Libro, Long> implements LibroService {
     public LibroServiceImpl(BaseRepository<Libro, Long> baseRepository) {
         super(baseRepository);
